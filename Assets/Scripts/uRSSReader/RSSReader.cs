186a5d1e72530bf1f40918fc9734d00a3db1696e
cpasjuste <cpasjuste@gmail.com>
2020-09-30 18:00:16 +0200

    RSSReader: use FeedReader ..

---
+++ Assets/Scripts/uRSSReader/RSSReader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using CodeHollow.FeedReader;
+
+namespace uRSSReader
+{
+    public class RSSReader : MonoBehaviour
+    {
+        public string rssUrl;
+
+        void Start()
+        {
+            StartCoroutine(GetRequest(rssUrl));
+        }
+
+        IEnumerator GetRequest(string uri)
+        {
+            var feed = FeedReader.ReadAsync(uri).GetAwaiter().GetResult();
+            Debug.Log("Feed Title: " + feed.Title);
+            Debug.Log("Feed Description: " + feed.Description);
+            Debug.Log("Feed Image: " + feed.ImageUrl);
+            foreach (var item in feed.Items)
+            {
+                string desc = item.Description.Replace("&nbsp;", "");
+                Debug.Log(item.Title + " - " + item.Link + "\n" + desc);
+            }
+
+            yield return null;
+        }
+    }
+}
